ea21279d349e9296e201918950724566d117fd7d
zend <zendwang040302@outlook.com>
2024-04-02 08:12:45 +0800

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -1,27 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
+const (
+	httpProtocol = "HTTP"
+	httpVersion  = "1.1"
+	httpStatusOK = "200 OK"
+	doubleCrlf   = "\r\n\r\n"
+)
+
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		log.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	defer func() {
+		err := l.Close()
+		if err != nil {
+			log.Println("Error closing listener: ", err.Error())
+			return
+		}
+	}()
+
+	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		log.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			log.Println("Error closing connection: ", err.Error())
+			return
+		}
+	}()
+
+	okResponse := httpProtocol + " " + httpStatusOK + " " + httpVersion + doubleCrlf
+	_, err = conn.Write([]byte(okResponse))
+	if err != nil {
+		log.Println("Error writing response: ", err.Error())
 		os.Exit(1)
 	}
 }
