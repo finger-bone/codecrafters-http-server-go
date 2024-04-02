29c063a98e4f5aecbc31f78674f3bf811f45e731
zend <zendwang040302@outlook.com>
2024-04-02 08:25:23 +0800

    pass 3rd stage

--- app/server.go
+++ app/server.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"log"
+	"strings"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
-const (
-	httpProtocol = "HTTP"
-	httpVersion  = "1.1"
-	httpStatusOK = "200 OK"
-	doubleCrlf   = "\r\n\r\n"
-)
+const okResponseHead = "HTTP/1.1 200 OK"
+const crlf = "\r\n"
+const notFoundResponseHead = "HTTP/1.1 404 Not Found"
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -43,10 +42,38 @@ func main() {
 		}
 	}()
 
-	okResponse := httpProtocol + "/" + httpVersion + " " + httpStatusOK + " " + httpVersion + doubleCrlf
-	_, err = conn.Write([]byte(okResponse))
+	req, err := readConn(conn)
 	if err != nil {
-		log.Println("Error writing response: ", err.Error())
+		log.Println("Error reading connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	splitted := strings.Split(req, crlf)
+	startLine := strings.Split(splitted[0], " ")
+	_, path, _ := startLine[0], startLine[1], startLine[2]
+
+	if path == "/" {
+		writeConn(conn, okResponseHead+crlf+crlf)
+	} else {
+		writeConn(conn, notFoundResponseHead+crlf+crlf)
+	}
+}
+
+func readConn(conn net.Conn) (string, error) {
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		return "", err
+	}
+
+	return string(buf[:n]), nil
+}
+
+func writeConn(conn net.Conn, resp string) error {
+	_, err := conn.Write([]byte(resp))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
